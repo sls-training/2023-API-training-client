e62ec04f77517b99a92ff9627e762457c90f1732
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-12 13:02:00 +0900

    Add rescue_from for StandardError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from StandardError, with: :render_internal_server_error
+
+  private
+
+  def render_internal_server_error
+    render file: Rails.root.join('public/500.html'), status: :internal_server_error,
+           layout: false, content_type: 'text/html'
+  end
 end
