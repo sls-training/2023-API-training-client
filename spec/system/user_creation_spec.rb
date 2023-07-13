a41fc9ce0e434f1f1f37949abeb10fd7b16c0626
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-13 18:00:05 +0900

    Sort the user creation spec for understandability

--- spec/system/user_creation_spec.rb
+++ spec/system/user_creation_spec.rb
@@ -3,131 +3,116 @@
 require 'rails_helper'
 
 RSpec.describe 'User Creation', type: :system do
-  context 'APIサーバ上のユーザアカウントの作成に成功したとき' do
-    subject do
-      visit signup_url
+  subject do
+    visit signup_url
 
-      fill_in 'Name', with: Faker::Name.name
-      fill_in 'Email', with: Faker::Internet.email
-      fill_in 'Password', with: password
-      fill_in 'Password confirmation', with: password
-      click_button 'Sign up'
+    fill_in 'Name', with: name
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password_confirmation
+    click_button 'Sign up'
 
-      page
+    page
+  end
+
+  let(:name) { Faker::Name.name }
+  let(:email) { Faker::Internet.email }
+  let(:password) { Faker::Internet.password }
+  let(:password_confirmation) { password }
+
+  context 'フォームへの入力が全て正当であるとき' do
+    context 'APIサーバから成功レスポンスが返ってきたとき' do
+      before do
+        WebMock.stub_request(:post, File.join(Api::User.base_url, 'user'))
+          .to_return(
+            body:    '{}',
+            status:  200,
+            headers: { 'Content-Type' => 'application/json' }
+          )
+      end
+
+      it 'ログイン用のURLにリダイレクトする' do
+        expect(subject).to have_current_path login_path
+      end
+
+      it 'ユーザの作成に成功した旨を示すメッセージが表示される' do
+        expect(subject).to have_content 'Your user account was created successfully'
+      end
     end
 
-    let(:password) { Faker::Internet.password }
+    context 'APIサーバからエラーレスポンスが返ってきたとき' do
+      before do
+        WebMock.stub_request(:post, File.join(Api::User.base_url, 'user')).to_return status: 500
+      end
 
-    before do
-      WebMock.stub_request(:post, File.join(Api::User.base_url, 'user'))
-        .to_return(
-          body:    '{}',
-          status:  200,
-          headers: { 'Content-Type' => 'application/json' }
-        )
-    end
-
-    it 'ログイン用のURLにリダイレクトする' do
-      expect(subject).to have_current_path login_path
-    end
-
-    it 'ユーザの作成に成功した旨を示すメッセージが表示される' do
-      expect(subject).to have_content 'Your user account was created successfully'
+      it '500エラー用のページを表示する' do
+        expect(subject).to have_content 'sorry'
+      end
     end
   end
 
-  context 'リクエストのパラメータが不正であるとき' do
-    subject do
-      visit signup_url
+  context 'フォームへの入力に不正があるとき' do
+    context '各フォームへの入力が空であるとき' do
+      let(:name) { nil }
+      let(:email) { nil }
+      let(:password) { nil }
 
-      click_button 'Sign up'
+      before do
+        WebMock.stub_request(:post, File.join(Api::User.base_url, 'user'))
+          .to_return(
+            body:    {
+              status: 422,
+              title:  'Unprocessable Entity',
+              errors: [
+                { name: 'name', reason: "can't be blank" },
+                { name: 'email', reason: 'is invalid' },
+                { name: 'password', reason:  "can't be blank" }
+              ]
+            }.to_json,
+            status:  422,
+            headers: { 'Content-Type' => 'application/problem+json' }
+          )
+      end
 
-      page
+      it 'サインアップ用のページを表示する' do
+        subject
+
+        expect(page).to have_field 'Name'
+        expect(page).to have_field 'Email'
+        expect(page).to have_field 'Password'
+        expect(page).to have_field 'Password confirmation'
+      end
+
+      it 'パラメータに関するバリデーションエラーが表示される' do
+        subject
+
+        expect(page).to have_content "name can't be blank"
+        expect(page).to have_content 'email is invalid'
+        expect(page).to have_content "password can't be blank"
+      end
     end
 
-    before do
-      WebMock.stub_request(:post, File.join(Api::User.base_url, 'user'))
-        .to_return(
-          body:    {
-            status: 422,
-            title:  'Unprocessable Entity',
-            errors: [
-              { name: 'name', reason: "can't be blank" },
-              { name: 'email', reason: 'is invalid' },
-              { name: 'password', reason:  "can't be blank" }
-            ]
-          }.to_json,
-          status:  422,
-          headers: { 'Content-Type' => 'application/problem+json' }
-        )
-    end
+    context 'パスワードがパスワードの確認と一致しないとき' do
+      let(:password_confirmation) { Faker::Internet.password }
 
-    it 'サインアップ用のページを表示する' do
-      subject
+      before do
+        WebMock.stub_request(:post, File.join(Api::User.base_url, 'user')).to_return status: 200
+      end
 
-      expect(page).to have_field 'Name'
-      expect(page).to have_field 'Email'
-      expect(page).to have_field 'Password'
-      expect(page).to have_field 'Password confirmation'
-    end
+      it 'サインアップ用のページを表示する' do
+        subject
 
-    it 'パラメータに関するバリデーションエラーが表示される' do
-      subject
+        expect(page).to have_field 'Name'
+        expect(page).to have_field 'Email'
+        expect(page).to have_field 'Password'
+        expect(page).to have_field 'Password confirmation'
+      end
 
-      expect(page).to have_content "name can't be blank"
-      expect(page).to have_content 'email is invalid'
-      expect(page).to have_content "password can't be blank"
-    end
-  end
+      it 'パスワードがパスワードの確認と一致しない旨のバリデーションエラーが表示される' do
+        subject
 
-  context 'パスワードがパスワードの確認と一致しないとき' do
-    subject do
-      visit signup_url
-
-      fill_in 'Name', with: Faker::Name.name
-      fill_in 'Email', with: Faker::Internet.email
-      fill_in 'Password', with: Faker::Internet.password
-      fill_in 'Password confirmation', with: Faker::Internet.password
-      click_button 'Sign up'
-
-      page
-    end
-
-    before do
-      WebMock.stub_request(:post, File.join(Api::User.base_url, 'user')).to_return status: 200
-    end
-
-    it 'サインアップ用のページを表示する' do
-      subject
-
-      expect(page).to have_field 'Name'
-      expect(page).to have_field 'Email'
-      expect(page).to have_field 'Password'
-      expect(page).to have_field 'Password confirmation'
-    end
-
-    it 'パスワードがパスワードの確認と一致しない旨のバリデーションエラーが表示される' do
-      subject
-
-      expect(page).to have_content "password_confirmation doesn't match with password"
-    end
-  end
-
-  context 'クラインアントエラー以外のエラーが発生したとき' do
-    subject do
-      visit signup_url
-
-      click_button 'Sign up'
-
-      page
-    end
-
-    before do
-      WebMock.stub_request(:post, File.join(Api::User.base_url, 'user')).to_return status: 500
-    end
-
-    it '500エラー用のページを表示する' do
-      expect(subject).to have_content 'sorry'
+        expect(page).to have_content "password_confirmation doesn't match with password"
+      end
     end
   end
 end
