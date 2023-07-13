aeda6538a01d7ba6b90e9e86cbdc5da7276684d0
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-13 18:49:02 +0900

    Remove a global rescue_from statement for StandardError
    Rails Guide doesn't recommended it.
    https://guides.rubyonrails.org/action_controller_overview.html#rescue-from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  rescue_from StandardError, with: :render_internal_server_error
-
   private
 
   def render_internal_server_error
