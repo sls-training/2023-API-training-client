39d099abebb86d403cee8a2859fe048821cb6330
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-21 13:46:08 +0900

    Fix the invalid Authorization header's value format

--- app/models/api/files.rb
+++ app/models/api/files.rb
@@ -7,7 +7,7 @@ module Api
     before_request do |name, request|
       if name == :all
         access_token = request.get_params.delete :access_token
-        request.headers['Authorization'] = "Bearer: #{access_token}"
+        request.headers['Authorization'] = "Bearer #{access_token}"
       end
     end
   end
