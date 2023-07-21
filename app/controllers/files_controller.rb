68170ed0d3b3cb7f2a91dfc00014fb31bb68ce65
a5ob7r <12132068+a5ob7r@users.noreply.github.com>
2023-07-21 18:42:53 +0900

    Add the main logic to the index files action

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -3,14 +3,23 @@
 class FilesController < ApplicationController
   include Authenticatable
 
+  before_action :require_login
+
   def index
-    # TODO: Implement the main logic.
-    Api::Files.all access_token: current_access_token
+    response = Api::Files.all access_token: current_access_token
+
+    @files = response[:files]
   rescue Flexirest::HTTPClientException
     # TODO: Implement more accurate error handlings.
     logout
     redirect_to new_session_path, status: :see_other
   rescue Flexirest::HTTPServerException, Flexirest::TimeoutException, Flexirest::ConnectionFailedException
     render_internal_server_error
+  end
+
+  private
+
+  def require_login
+    redirect_to new_session_path unless logged_in?
   end
 end
